07500d68147d7da486fe4535fa408f950757de18
jeffinDev <jeffin9948@gmail.com>
2025-07-04 14:34:12 +0000

    atividadee

---
+++ atvd.py
@@ -0,0 +1,17 @@
+
+## PARES
+
+for numeropar in range(0, 21, 2):
+    print(numeropar)
+
+##IMPARES
+
+for numeroim in range(1, 43, 2):
+    print(numeroim)
+
+## fibonacci sequencia
+
+a, b = 0, 1
+while a <= 20:
+    print(a)
+    a, b = b, a + b
